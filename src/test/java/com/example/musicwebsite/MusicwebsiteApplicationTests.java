0b1bc4204a6a36a25f93a6aac9bd90080fc08a02
gaotaojiao <2597586064@qq.com>
2025-03-10 14:27:15 +0800

    Initial commit

---
+++ src/test/java/com/example/musicwebsite/MusicwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicwebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
